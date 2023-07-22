1b0fab1515df2fc56ff45fd47bc40604058b1a77
wangpeng <841987035@qq.com>
2023-07-22 17:31:37 +0800

    重新编译了IJKMediaFramework，此版本为openssl版本的库，支持了https

--- Example/IJKMediaFramework/WHViewController.m
+++ Example/IJKMediaFramework/WHViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "WHViewController.h"
+#import <IJKMediaFramework/IJKMediaFramework.h>
 
 @interface WHViewController ()
+
+@property (nonatomic, strong) IJKFFMoviePlayerController *player;
 
 @end
 
@@ -17,7 +20,45 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+	NSURL *assetURL = [NSURL URLWithString:@"https://www.apple.com/105/media/us/iphone-x/2017/01df5b43-28e4-4848-bf20-490c34a926a7/films/feature/iphone-x-feature-tpl-cc-us-20170912_1280x720h.mp4"];
+	
+#ifdef DEBUG
+	[IJKFFMoviePlayerController setLogReport:YES];
+	[IJKFFMoviePlayerController setLogLevel:k_IJK_LOG_DEBUG];
+#else
+	[IJKFFMoviePlayerController setLogReport:NO];
+	[IJKFFMoviePlayerController setLogLevel:k_IJK_LOG_INFO];
+#endif
+
+	[IJKFFMoviePlayerController checkIfFFmpegVersionMatch:YES];
+	// [IJKFFMoviePlayerController checkIfPlayerVersionMatch:YES major:1 minor:0 micro:0];
+
+	IJKFFOptions *options = [IJKFFOptions optionsByDefault];
+
+	self.player = [[IJKFFMoviePlayerController alloc] initWithContentURL:assetURL withOptions:options];
+	self.player.view.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+	self.player.view.frame = self.view.bounds;
+	self.player.scalingMode = IJKMPMovieScalingModeAspectFit;
+	self.player.shouldAutoplay = YES;
+
+	self.view.autoresizesSubviews = YES;
+	[self.view addSubview:self.player.view];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+	[super viewWillAppear:animated];
+
+	[self.player prepareToPlay];
+	[self.player play];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+	
+	[self.player shutdown];
 }
 
 - (void)didReceiveMemoryWarning
--- IJKMediaFramework.podspec
+++ IJKMediaFramework.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'IJKMediaFramework'
-  s.version          = '0.1.2'
+  s.version          = '0.1.3'
   s.summary          = 'ijkplayer framework.'
 
 # This description is used to generate tags and improve search results.
